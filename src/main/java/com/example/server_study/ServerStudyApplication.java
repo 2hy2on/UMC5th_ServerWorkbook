af23b90c30d4ff9159f86c8980ada275d97d72e6
2hy2on <mashin0128@gmail.com>
2024-01-23 01:12:34 +0900

    ãnew

---
+++ src/main/java/com/example/server_study/ServerStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/server_study/ServerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
